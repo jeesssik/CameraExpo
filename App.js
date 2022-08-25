094481e773b65d4e212ac93745aa54d82dda093a
Android Studio <devandroid@ort>
2022-08-25 14:09:05 -0300

    13.45

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+import React, {useState, useEffect, useRef} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Camera, CameraType} from 'expo-camera'
+import * as MediaLibrary from 'expo-media-library'
+import React, { useState } from 'react';
+
 
 export default function App() {
+  
+  const [hasCameraPermission, setHasCameraPermission]= useState(null);
+  const [image, setImage]= useState(null);
+  const [type, setType]= useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() =>{
+    (async () =>{
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    }) ();
+  },[])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        flashMode={flash}
+        ref={cameraRef}
+     >
+      <Text>Helllloooo</Text>
+      </Camera>
+      <View>
+        
+      </View>
     </View>
   );
 }
@@ -14,7 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  camera:{
+    flex:1,
+    borderRadius:20,
+  }
+
 });
